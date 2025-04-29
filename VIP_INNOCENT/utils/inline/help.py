d3289b49b7ebbe035bcaf8bb74dc5cd9be6946c6
Boorip <209272077+Boorip@users.noreply.github.com>
2025-04-29 12:30:58 +0530

    Update help.py

--- VIP_INNOCENT/utils/inline/help.py
+++ VIP_INNOCENT/utils/inline/help.py
@@ -1,167 +1,58 @@
-from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from VIP_INNOCENT import app
 
-def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")]
-    second = [
-        InlineKeyboardButton(
-            text=_["BACK_PAGE"],
-            callback_data=f"mbot_cb",
-        ),
-        InlineKeyboardButton(
-            text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
-        ),InlineKeyboardButton(
-            text=_["NEXT_PAGE"],
-            callback_data=f"mbot_cb",
-        ),
-    ]
-    mark = second if START else first
-    
-    upl = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(text=_["H_B_1"], callback_data="help_callback hb1"),
-                InlineKeyboardButton(text=_["H_B_2"], callback_data="help_callback hb2"),
-                InlineKeyboardButton(text=_["H_B_3"], callback_data="help_callback hb3"),
-            ],
-            [
-                InlineKeyboardButton(text=_["H_B_4"], callback_data="help_callback hb4"),
-                InlineKeyboardButton(text=_["H_B_5"], callback_data="help_callback hb5"),
-                InlineKeyboardButton(text=_["H_B_6"], callback_data="help_callback hb6"),
-            ],
-            [
-                InlineKeyboardButton(text=_["H_B_7"], callback_data="help_callback hb7"),
-                InlineKeyboardButton(text=_["H_B_8"], callback_data="help_callback hb8"),
-                InlineKeyboardButton(text=_["H_B_9"], callback_data="help_callback hb9"),
-            ],
-            [
-                InlineKeyboardButton(text=_["H_B_10"], callback_data="help_callback hb10"),
-                InlineKeyboardButton(text=_["H_B_11"], callback_data="help_callback hb11"),
-                InlineKeyboardButton(text=_["H_B_12"], callback_data="help_callback hb12"),
-            ],
-            [
-                InlineKeyboardButton(text=_["H_B_13"], callback_data="help_callback hb13"),
-                InlineKeyboardButton(text=_["H_B_14"], callback_data="help_callback hb14"),
-                InlineKeyboardButton(text=_["H_B_15"], callback_data="help_callback hb15"),
-            ],
-            [InlineKeyboardButton(text=_["C_B_3"], callback_data="help_callback cbot"),],
-            mark,
-        ]
-    )
-    return upl
+def generate_help_buttons(_, start: int, end: int, current_page: int):
+    buttons = []
+    buttons_per_row = 3
+    for idx, i in enumerate(range(start, end + 1)):
+        if idx % buttons_per_row == 0:
+            buttons.append([])
+        buttons[-1].append(InlineKeyboardButton(
+            text=_[f"H_B_{i}"],
+            callback_data=f"help_callback hb{i}_p{current_page}"
+        ))
+    return buttons
 
-def help_back_markup(_):
-    upl = InlineKeyboardMarkup(
+def first_page(_):
+    buttons = generate_help_buttons(_, start=1, end=15, current_page=1)
+    navigation_buttons = [
+        InlineKeyboardButton(text="ʜᴏᴍᴇ", callback_data="back_to_main"),
+        InlineKeyboardButton(text="ɴᴇxᴛ »", callback_data="help_next_2")
+    ]
+    buttons.append(navigation_buttons)
+    return InlineKeyboardMarkup(buttons)
+
+def second_page(_):
+    buttons = generate_help_buttons(_, start=16, end=30, current_page=2)
+    navigation_buttons = [
+        InlineKeyboardButton(text="« ʙᴀᴄᴋ", callback_data="help_prev_1"),
+        InlineKeyboardButton(text="ʜᴏᴍᴇ", callback_data="back_to_main")
+    ]
+    buttons.append(navigation_buttons)
+    return InlineKeyboardMarkup(buttons)
+
+def help_back_markup(_, current_page):
+    return InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
-                    callback_data="settings_back_helper",
+                    callback_data=f"help_back_{current_page}"
+                ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close"
                 ),
             ]
         ]
     )
-    return upl
 
 def private_help_panel(_):
-    buttons = [
+    return [
         [
             InlineKeyboardButton(
                 text=_["S_B_4"],
-                url=f"https://t.me/{app.username}?start=help",
+                url=f"https://t.me/{app.username}?start=help"
             ),
         ],
     ]
-    return buttons
-
-
-def first_page(_):
-    controll_button = [
-        InlineKeyboardButton(text="๏ ᴍᴇɴᴜ ๏", callback_data=f"settingsback_helper"),
-        InlineKeyboardButton(text="๏ ɴᴇxᴛ ๏", callback_data=f"dilXaditi"),
-    ]
-    first_page_menu = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(text=_["H_B_1"], callback_data="help_callback hb1"),
-                InlineKeyboardButton(text=_["H_B_2"], callback_data="help_callback hb2"),
-                InlineKeyboardButton(text=_["H_B_3"], callback_data="help_callback hb3"),
-            ],
-            [
-                InlineKeyboardButton(text=_["H_B_11"], callback_data="help_callback hb11"),
-                InlineKeyboardButton(text=_["H_B_8"], callback_data="help_callback hb8"),
-                InlineKeyboardButton(text=_["H_B_6"], callback_data="help_callback hb6"),
-            ],
-            [
-                InlineKeyboardButton(text=_["H_B_13"], callback_data="help_callback hb13"),
-                InlineKeyboardButton(text=_["H_B_12"], callback_data="help_callback hb12"),
-                InlineKeyboardButton(text=_["H_B_9"], callback_data="help_callback cloghelp"),
-            ],
-            [
-                InlineKeyboardButton(text=_["H_B_10"], callback_data="help_callback hb10"),
-                InlineKeyboardButton(text=_["H_B_14"], callback_data="help_callback hb14"),
-                InlineKeyboardButton(text=_["H_B_15"], callback_data="help_callback hb15"),
-            ],
-            [
-                InlineKeyboardButton(text=_["C_B_1"], callback_data="help_callback chelp"),
-            ],
-            [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")]
-        ]
-    )
-    return first_page_menu
-
-def second_page(_):
-    controll_button = [
-        InlineKeyboardButton(text="๏ ʙᴀᴄᴋ ๏", callback_data=f"settings_back_helper")
-    ]
-    second_page_menu = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_7"], callback_data="help_callback hb7"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_19"], callback_data="help_callback hb19"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_14"], callback_data="help_callback hb14"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_15"], callback_data="help_callback hb15"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_16"], callback_data="help_callback hb16"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_17"], callback_data="help_callback hb17"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_18"], callback_data="help_callback hb18"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_13"], callback_data="help_callback hb13"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_20"], callback_data="help_callback hb20"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_22"], callback_data="help_callback hb22"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_21"], callback_data="help_callback hb21"
-                )
-            ],
-            controll_button,
-        ]
-    )
-    return second_page_menu
